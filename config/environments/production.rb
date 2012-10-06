599ead5884394bbdf624547ecb83a2e2042f52ab
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-06 17:31:06 -0300

    added the google analytics tracker id

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 JosemarLuedke::Application.configure do
   # Google Analytics
-  config.middleware.use Rack::GoogleAnalytics, :tracker => 'YOUR-ANALYTICS-CODE'
+  config.middleware.use Rack::GoogleAnalytics, :tracker => 'UA-13128274-4'
 
   # Settings specified here will take precedence over those in config/application.rb
 
