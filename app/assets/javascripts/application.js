5bcf30140c7836b250bded71d6267fb1d21ed65d
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-04-20 23:17:49 -0300

    Removes backbone and underscore

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,3 @@
 //
 //= require jquery
 //= require jquery_ujs
-
-//= require ./lib/underscore.js
-//= require ./lib/backbone.js
-//= require_tree ./lib
-//= require ./app.js
-//= require_tree ./app
-//= require init.js
-
